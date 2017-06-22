e51f864a49e84ac591b5d5b5f756d71868200177
Paul McLear <paulmclear@pauls-mbp.home>
2017-06-22 20:54:29 +0100

    Added .gitignore and alembic files.

---
+++ alembic/versions/f61eb23a21fc_first_revision.py
@@ -0,0 +1,24 @@
+"""First revision.
+
+Revision ID: f61eb23a21fc
+Revises: 
+Create Date: 2017-06-22 20:35:57.601286
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f61eb23a21fc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
