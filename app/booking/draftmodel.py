a79a83d5457543be81791ae2e77e7ee60696683a
pemm8 <pemm8@users.noreply.github.com>
2017-09-07 12:54:52 +0100

    Revised timeblock model and added calculate_timeblock function

--- app/booking/draftmodel.py
+++ app/booking/draftmodel.py
@@ -1,12 +1,4 @@
-class Booking(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	datetime_start = db.Column(db.Datetime)
-	datetime_end = db.Column(db.Datetime)
-	datetime_request = db.Column(db.Datetime)
-	status = db.Column(db.String)
-	datetime_status = db.Column(db.Datetime)
-	requestor_id = db.Column(db.Integer, db.ForeignKey('client.id'))
-	note = db.Column(db.Text)
+import datetime
 
 class Client(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -17,14 +9,29 @@ class Client(db.Model):
 	def __str__(self):
 		return self.email
 
-class Availability(db.Model):
+class Booking(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	date = db.Column(db.Date)
-	timeblock = db.Column(db.Datetime)
+	datetime_start = db.Column(db.Datetime)
+	datetime_end = db.Column(db.Datetime)
+	datetime_request = db.Column(db.Datetime)
 	status = db.Column(db.String)
-	booking_id = db.Column(db.Integer, db.ForeignKey('booking.id'))
+	datetime_status = db.Column(db.Datetime)
+	requestor_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+	note = db.Column(db.Text)
 
+class Timeblock(db.Model):
+	id = db.Column(db.Double, primary_key=True)
+	status = db.Column(db.Integer)
+	booking_id = db.Colmun(db.Integer, db.ForeignKey('booking.id'))
+
+	def __str__(self):
+		return self.id
+	
 def check_available(timeblock_start,timeblock_end):
+	# TODO
+
+def calculate_timeblock(date):
+	return datetime.strftime('%Y%m%d%H%M',date)
 
 
 
